b17cff3a4933b02f34ccd42ef2e184a6ed7139a7
boistei <29461756+boistei@users.noreply.github.com>
2023-03-05 17:03:40 +0530

    Add reset functionality. Styling pending

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-let score = document.querySelector("#score");
+let p1ScoreDisp = document.querySelector("#score1");
+let p2ScoreDisp = document.querySelector("#score2");
 let maxScore = document.querySelector("#maxScore");
 
 const p1Button = document.querySelector("#player1");
 const p2Button = document.querySelector("#player2");
+const reset = document.querySelector("#reset");
 let p1Score = 0;
 let p2Score = 0;
 
 p1Button.addEventListener("click", (e) => {
     p1Score++;
-    score.textContent = `${p1Score} : ${p2Score}`;
+    p1ScoreDisp.textContent = p1Score;
+    p2ScoreDisp.textContent = p2Score;
     if (p1Score >= parseInt(maxScore.value)) {
         p1Button.disabled = true;
         p2Button.disabled = true;
@@ -17,9 +20,19 @@ p1Button.addEventListener("click", (e) => {
 
 p2Button.addEventListener("click", (e) => {
     p2Score++;
-    score.textContent = `${p1Score} : ${p2Score}`;
+    p1ScoreDisp.textContent = p1Score;
+    p2ScoreDisp.textContent = p2Score;
     if (p2Score >= parseInt(maxScore.value)) {
         p2Button.disabled = true;
         p2Button.disabled = true;
     }
+})
+
+reset.addEventListener("click", (e) => {
+    p1Score = 0;
+    p2Score = 0;
+    p1ScoreDisp.textContent = p1Score;
+    p2ScoreDisp.textContent = p2Score;
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 })
