c37a496224a4bcf47d40142e7c33c3422a864069
boistei <29461756+boistei@users.noreply.github.com>
2023-03-10 19:57:19 +0530

    Change CSS framework to bootstrap

--- app.js
+++ app.js
@@ -3,55 +3,31 @@ const distance = document.querySelector("#distance");
 
 let count = 0;
 let dist = 0;
+let lapsPerKM = 4.5;
 
 const addQrtr = document.querySelector("#addQuarter");
 const addHalf = document.querySelector("#addHalf");
 const addFull = document.querySelector("#addFull");
 
-const remQrtr = document.querySelector("#remQuarter");
-const remHalf = document.querySelector("#remHalf");
-const remFull = document.querySelector("#remFull");
-
 const reset = document.querySelector("#reset");
 
 addQrtr.addEventListener("click", (e) => {
     count += 0.25;
-    dist += (0.25 / 4.5);
+    dist += (0.25 / lapsPerKM);
     counter.textContent = `${count}`;
     distance.textContent = Math.floor(dist * 100) / 100;
 });
 
 addHalf.addEventListener("click", (e) => {
     count += 0.5;
-    dist += (0.5 / 4.5);
+    dist += (0.5 / lapsPerKM);
     counter.textContent = `${count}`;
     distance.textContent = Math.floor(dist * 100) / 100;
 });
 
 addFull.addEventListener("click", (e) => {
     count += 1;
-    dist += (1 / 4.5);
-    counter.textContent = `${count}`;
-    distance.textContent = Math.floor(dist * 100) / 100;
-});
-
-remQrtr.addEventListener("click", (e) => {
-    count -= 0.25;
-    dist -= (0.25 / 4.5);
-    counter.textContent = `${count}`;
-    distance.textContent = Math.floor(dist * 100) / 100;
-});
-
-remHalf.addEventListener("click", (e) => {
-    count -= 0.5;
-    dist -= (0.5 / 4.5);
-    counter.textContent = `${count}`;
-    distance.textContent = Math.floor(dist * 100) / 100;
-});
-
-remFull.addEventListener("click", (e) => {
-    count -= 1;
-    dist -= (1 / 4.5);
+    dist += (1 / lapsPerKM);
     counter.textContent = `${count}`;
     distance.textContent = Math.floor(dist * 100) / 100;
 });
